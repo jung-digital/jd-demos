26b73b7f2a4f646a9243f1d0d35fcaaf11818a92
joshjung <joshua.p.jung@gmail.com>
2015-08-11 21:45:48 -0500

    Cleaning up the Header.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,13 +14,12 @@ class Header {
       <div className="Header">
         <div className="Header-container">
           <a className="Header-brand" href="/" onClick={Link.handleClick}>
-            <img className="Header-brandImg" src={require('./logo-small.png')} width="38" height="38" alt="React" />
-            <span className="Header-brandTxt">Your Company</span>
+            <img className="Header-brandImg" src={require('./logo-white-small.png')} height="38" alt="Jung Digital LLC" />
           </a>
           <Navigation className="Header-nav" />
           <div className="Header-banner">
-            <h1 className="Header-bannerTitle">React</h1>
-            <p className="Header-bannerDesc">Complex web apps made easy</p>
+            <h1 className="Header-bannerTitle">JavaScript Demos</h1>
+            <p className="Header-bannerDesc">Built with React.js, Gulp, and Canvas</p>
           </div>
         </div>
       </div>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -16,12 +16,7 @@ class Navigation {
   render() {
     return (
       <div className={classNames(this.props.className, 'Navigation')} role="navigation">
-        <a className="Navigation-link" href="/about" onClick={Link.handleClick}>About</a>
-        <a className="Navigation-link" href="/contact" onClick={Link.handleClick}>Contact</a>
-        <span className="Navigation-spacer"> | </span>
-        <a className="Navigation-link" href="/login" onClick={Link.handleClick}>Log in</a>
-        <span className="Navigation-spacer">or</span>
-        <a className="Navigation-link Navigation-link--highlight" href="/register" onClick={Link.handleClick}>Sign up</a>
+        <a className="Navigation-link" href="http://www.jungdigital.com">Home</a>
       </div>
     );
   }
