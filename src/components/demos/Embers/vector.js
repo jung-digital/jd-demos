f3b6940fa6e48390973bcac0790f237863ff6ed7
Thomas Yarnall <corragon@gmail.com>
2015-09-23 11:28:27 -0500

    Added angle and magnitude properties

--- src/components/demos/Embers/vector.js
+++ src/components/demos/Embers/vector.js
@@ -1,8 +1,28 @@
 
 class Vector {
-  Vector(x, y) {
+  constructor(x, y) {
     this.x = x_;
     this.y = y_;
+    this.angle = {
+      get angle () {
+        return Math.atan2(this.x, this.y);
+      },
+      set angle (val) {
+        var l = Math.sqrt(this.x * this.x + this.y * this.y);
+        this.x = Math.cos(val) * l;
+        this.y = Math.sin(val) * l;
+      }
+    }
+    this.magnitude = {
+      get magnitude () {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+      },
+      set magnitude (val) {
+        var angle = Math.atan2(this.x, this.y);
+        this.x = Math.cos(angle) * val;
+        this.y = Math.sin(angle) * val;
+      }
+    }
   }
   add(vector) {
     this.x += vector.x;
@@ -11,5 +31,9 @@ class Vector {
   sub(vector) {
     this.x -= vector.x;
     this.y -= vector.y;
+  }
+  multiply(scalar) {
+    this.x *= scalar;
+    this.y *= scalar;
   }
 }
